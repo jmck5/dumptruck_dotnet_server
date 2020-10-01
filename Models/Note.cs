9597a08ba6423f67463d7a26928ecb6dc317a123
Jmck074 <jmck074@aucklanduni.ac.nz>
2020-10-01 17:04:05 +1300

    Add project files.

---
+++ Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dumptruck.Models
+{
+    public class Note
+    {
+        public long NoteId { get; set; }
+        public string NoteContent { get; set; }
+        public string NoteAuthor { get; set; }
+        public TimestampAttribute NoteTimeStamp { get; set; }
+
+    }
+}
